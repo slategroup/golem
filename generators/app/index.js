fc5dbe7173045d2e48232e6e100e677999e24d61
Salomone Baquis <salomone.l.baquis@gmail.com>
2019-06-24 11:21:19 -0400

    removed paths()

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,30 +1,24 @@
-const Generator = require('yeoman-generator')
-const path = require("path")
-const promptPath = path.resolve(__dirname, "prompts/prompts.js")
-const { namePrompt, descriptionPrompt } = require(promptPath)
+const Generator = require("yeoman-generator");
+const path = require("path");
+const promptPath = path.resolve(__dirname, "prompts/prompts.js");
+const { namePrompt, descriptionPrompt } = require(promptPath);
 
 module.exports = class extends Generator {
   constructor(args, opts) {
-    super(args, opts)
+    super(args, opts);
   }
 
   async prompting() {
-    this.answers = await this.prompt([
-      namePrompt,
-      descriptionPrompt
-    ])
-  }
-
-  paths() {
-    this.destinationRoot(this.answers.componentName)
+    this.answers = await this.prompt([namePrompt, descriptionPrompt]);
   }
 
   writing() {
+    this.destinationRoot(this.answers.componentName);
+
     this.fs.copyTpl(
       this.templatePath("**/*"),
       this.destinationPath(),
       this.answers
-    )
+    );
   }
-}
-
+};
